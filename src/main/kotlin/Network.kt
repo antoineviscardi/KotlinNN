c5b4258f1f41fffd911ab771bb3b21e4ecb15ee8
antoineviscardi <antoine.viscardi@gmail.com>
2019-02-15 23:24:08 -0500

    Implemented cost function and currently started implementing backprop.

--- src/main/kotlin/Network.kt
+++ src/main/kotlin/Network.kt
@@ -1,4 +1,5 @@
 import org.nd4j.linalg.api.ndarray.INDArray
+import org.nd4j.linalg.dataset.DataSet
 import org.nd4j.linalg.indexing.NDArrayIndex
 import org.nd4j.linalg.factory.Nd4j as nd
 import org.nd4j.linalg.ops.transforms.Transforms.*
@@ -17,19 +18,55 @@ class Network(private vararg val dimensions: Int) {
         return activations.last()
     }
 
+    fun mse(pred: INDArray, label: INDArray): INDArray {
+        val diff = pred.sub(label)
+        val result = pow(diff, 2)
+        return result
+    }
+
+    fun dMse(pred: INDArray, label: INDArray): INDArray {
+        return pred.sub(label)
+    }
+
+    fun dSoftmax(a: INDArray): INDArray {
+        // TODO: Implement this!
+        return nd.empty()
+    }
+
+    fun dSigmoid(a: INDArray): INDArray {
+        return sigmoid(a).mul(sigmoid(a).rsub(1))
+    }
+
+    fun backprop(learningRate: Double) {
+        for (i in dimensions.size - 1 downTo 0) {
+
+        }
+    }
+
+    fun train(trainSet: DataSet) {
+
+    }
+
 
 }
 
 fun main() {
     nd.getRandom().setSeed(42)
 
-    val data = nd.readNumpy("data/wine.data", ",")
-    println(data.get(NDArrayIndex.interval(0, 10)))
+//    val m1 = nd.create(doubleArrayOf(1.0, 2.0, 3.0, 4.0))
+//    println(pow(m1, 2))
 
+    // Fetch preprocessed data
+    val testAndTrain = Preprocessor("wine.data").getPreprocessedData()
+    val trainSet = testAndTrain.train
+    val testSet = testAndTrain.test
+
+
+    // Instantiate network
     val network = Network(2, 3, 2)
-    val input = nd.ones(2, 1)
-    val out = network.feedForward(input)
-    print(out)
+
+
+
 }
 
 //[0.8053,
