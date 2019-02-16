bc9e0f02027841916cd922b8f297ba2109452ab5
antoineviscardi <antoine.viscardi@gmail.com>
2019-02-15 23:23:13 -0500

    Finished implementing preprocessing

--- src/main/kotlin/Preprocessor.kt
+++ src/main/kotlin/Preprocessor.kt
@@ -1,21 +1,19 @@
-import org.deeplearning4j.datasets.datavec.RecordReaderDataSetIterator
-import org.nd4j.linalg.api.ndarray.INDArray
 import org.nd4j.linalg.dataset.DataSet
+import org.nd4j.linalg.dataset.SplitTestAndTrain
 import org.nd4j.linalg.dataset.api.preprocessor.NormalizerStandardize
 import org.nd4j.linalg.factory.Nd4j
 import org.nd4j.linalg.indexing.NDArrayIndex
 
-class Preprocessor (val path: String) {
+class Preprocessor (val file: String) {
 
-    private val rawData = Nd4j.readNumpy(ClassLoader.getSystemResource("wine.data").path, ",")
+    private val rawData = Nd4j.readNumpy(ClassLoader.getSystemResource(file).path, ",")
     private val dataset = DataSet(rawData.get(NDArrayIndex.all(), NDArrayIndex.interval(1, rawData.columns())), rawData.getColumn(0))
 
-    fun foo() {
+    fun getPreprocessedData(): SplitTestAndTrain {
 
         // One-hot encode labels
         val nClass = dataset.labels.maxNumber().toInt()
         for (i in 1..nClass) {
-            println(Nd4j.hstack(dataset.labels, dataset.labels.getColumn(0).eq(i)))
             dataset.labels = Nd4j.hstack(dataset.labels, dataset.labels.getColumn(0).eq(i))
         }
         dataset.labels = dataset.labels.getColumns(*IntArray(nClass) {it + 1})
@@ -26,19 +24,13 @@ class Preprocessor (val path: String) {
 
         // Split into train and test sets
         val testAndTrain = dataset.splitTestAndTrain(0.70)
-        val trainSet = testAndTrain.train
-        val testSet = testAndTrain.test
 
         // Normalize data
         val normalizer = NormalizerStandardize()
-        normalizer.fit(trainSet)
-        normalizer.transform(trainSet)
-        normalizer.transform(testSet)
+        normalizer.fit(testAndTrain.train)
+        normalizer.transform(testAndTrain.train)
+        normalizer.transform(testAndTrain.test)
 
+        return testAndTrain
     }
-}
-
-fun main() {
-    val preprocessor = Preprocessor("")
-    preprocessor.foo()
 }
